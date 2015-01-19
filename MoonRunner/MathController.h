59f90ada0b2ffaab347810ec5226ecb1dd553f00
Jonathan Kim <jonnokim17@gmail.com>
2015-01-18 20:49:06 -0800

    MathController Class created

---
+++ MoonRunner/MathController.h
@@ -0,0 +1,17 @@
+//
+//  MathController.h
+//  MoonRunner
+//
+//  Created by Jonathan Kim on 1/18/15.
+//  Copyright (c) 2015 Jonathan Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MathController : NSObject
+
++ (NSString *)stringifyDistance:(float)meters;
++ (NSString *)stringifySecondCount:(int)seconds usingLongFormat:(BOOL)longFormat;
++ (NSString *)stringifyAvgPaceFromDist:(float)meters overTime:(int)seconds;
+
+@end
